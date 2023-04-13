3008d384b1bd463bc5d5ec7ac877fc9707938596
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-04-13 13:51:48 +0200

    Update perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,65 +1,54 @@
 import numpy as np
 
+
 class Perceptron:
-    def __init__(self,size) -> None:
+    def __init__(self) -> None:
         self.m_inputs = None 
-        self.m_weights = None
-        self.m_bias = None
         self.m_features = 0
+        self.m_bias = None
+        self.trained = None #????
 
-    def stepFunction(self,x):
-        return np.where(x > 0 , 1, 0)
-
-    def fit(self, X, y, epochs = 100, alpha = .01):
+    @staticmethod
+    def stepFunction(x):
+        return np.where(x > 0, 1, 0) 
+    
+    def train(self, X, y, epochs = 100, alpha = .01, af = stepFunction) -> float:
         samples, self.m_features = X.shape
-        print(samples,self.m_features)
         self.m_weights = np.random.rand(self.m_features)
         self.m_bias = np.random.rand()
+        for _ in range(epochs):
+            for _x,_y in zip(X,y):  
 
-        yn = np.where(y > 0 , 1, 0)
-
-        for ep in range(epochs):
-            for i, x in enumerate(X):  
-
-                yd = self.predict(x) 
-                w = alpha * (y[i] - yd)
+                e = _y - self.predict(_x) 
+                w = alpha * e
                 self.m_bias += w
-                self.m_weights += w*x
+                self.m_weights += w*_x
 
+    def predict(self,x,af = stepFunction) -> float:
+        return af(np.dot(x, self.m_weights) + self.m_bias)
 
-        print(self.m_weights, self.m_bias)
-
-        
-
-    def predict(self, X):
-        
-        return self.stepFunction(np.dot(X, self.m_weights) + self.m_bias)
-
-
-
+#-----------------------------------------------------------------------------------------
 
 
 def main():
     import matplotlib.pyplot as plt
     from sklearn.model_selection import train_test_split
     from sklearn import datasets
-    X, y = datasets.make_blobs(
-        n_samples=200, n_features=2, centers=2, cluster_std=1.05, random_state=2
-    )
+    X, y = datasets.make_blobs(n_samples=200, n_features=2, centers=2, cluster_std=2.5)
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=170
-    )
+        X, y, test_size=0.3)
 
-    network = Perceptron(5)
-    network.fit(X_train,y_train)
+    network = Perceptron()
+    network.train(X_train,y_train)
 
     predictions = network.predict(X_test)
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    plt.scatter(X_train[:, 0], X_train[:, 1], marker=".", c=y_train)
 
+    plt.scatter(X_train[:, 0], X_train[:, 1], marker=".", c = y_train, cmap="viridis")
+    plt.scatter(X_test[:, 0],X_test[:, 1], marker="*",c = -predictions, cmap="coolwarm")
     x0_1 = np.amin(X_train[:, 0])
     x0_2 = np.amax(X_train[:, 0])
 
@@ -67,7 +56,8 @@ def main():
     x1_2 = (-network.m_weights[0] * x0_2 - network.m_bias) / network.m_weights[1]
     ax.plot([x0_1, x0_2], [x1_1, x1_2])
 
+
     plt.show()
 
 if __name__ == "__main__":
-      main()
+    main()
